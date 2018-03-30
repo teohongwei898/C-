1a17195371ba883a228a8340c501f643e741896c
Hong Wei <teohongwei898@gmail.com>
2018-03-30 22:01:21 +0800

    Update leapyearchecker.cpp
    Added on how leap year works

--- leapyearchecker.cpp
+++ leapyearchecker.cpp
@@ -1,3 +1,11 @@
+/*
+To check:
+1.If the year is evenly divisible by 4, go to step 2. ...
+2.If the year is evenly divisible by 100, go to step 3. ...
+3.If the year is evenly divisible by 400, go to step 4. ...
+The year is a leap year (it has 366 days).
+The year is not a leap year (it has 365 days).
+*/
 #include <iostream>
 using namespace std;
 
