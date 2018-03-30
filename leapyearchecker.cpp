363231a384b9bc68858707d0c571e66dc05a3da0
Hong Wei <teohongwei898@gmail.com>
2018-03-30 21:59:34 +0800

    Create leapyearchecker.cpp
    Leap Year Checker

---
+++ leapyearchecker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int year
+    cout << "Enter year to check whether it is a leap year!"<<endl;
+    cin >> year
+    if (year&4==0)
+    {
+      if (year%100==0)
+      {
+        if (year%400==0)
+        {
+        cout<< "The year you have keyed in is a leap year." <<endl;
+        }
+      }
+     }
+     else
+     {
+     cout << "The year you have keyed in is not a leap year."<<endl;
+      
+    return 0;
+}
