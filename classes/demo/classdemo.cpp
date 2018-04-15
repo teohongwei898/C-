ae75ea633cb198df1408958b4c7694e4959ab0b4
Hong Wei <teohongwei898@gmail.com>
2018-04-15 11:57:35 +0800

    Create classdemo.cpp

---
+++ classes/demo/classdemo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class demoClass{
+  public  ;
+      void helloworld(){
+        cout<< "Hello World"<< endl;
+        }
+  };      
+int main() 
+{
+  demoClass demoObject;
+  demoObject.helloworld();
+    return 0;
+    }
